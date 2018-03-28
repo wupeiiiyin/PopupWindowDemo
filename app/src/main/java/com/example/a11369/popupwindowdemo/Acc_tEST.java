76e7c3a89cecfa221eef72d900e6e1fc9ed3d23d
吴老师 <zlb_00790@126.com>
2018-03-28 13:10:51 +0800

    第一次提交 新增一个类 2018-3-28 13:10:43

---
+++ app/src/main/java/com/example/a11369/popupwindowdemo/Acc_tEST.java
@@ -0,0 +1,26 @@
+package com.example.a11369.popupwindowdemo;
+
+/**
+ * Created by 11369 on 2018/3/28.
+ */
+
+public class Acc_tEST {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
